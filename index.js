caa21b80cce9383a1a7343d4ac6c5eaacfa7edac
Oronno Akter Usha <ushaoronno1@gmail.com>
2024-12-02 20:56:55 +0600

    feat: add auth related api and logout

--- index.js
+++ index.js
@@ -62,6 +62,21 @@ async function run() {
         })
         .send({ success: true });
     });
+    // Logout
+    app.get("/logout", async (req, res) => {
+      try {
+        res
+          .clearCookie("token", {
+            maxAge: 0,
+            secure: process.env.NODE_ENV === "production",
+            sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+          })
+          .send({ success: true });
+        console.log("Logout successful");
+      } catch (err) {
+        res.status(500).send(err);
+      }
+    });
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
